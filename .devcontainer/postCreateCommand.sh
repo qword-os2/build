d49a4c494f5d25bb400367ddd774b1ae9208bd27
ajh123 <41990982+ajh123@users.noreply.github.com>
2023-07-12 11:32:48 +0000

    Simply container

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,3 @@
 sudo apt update
 sudo apt install -y wget git bash make patch gzip tar nasm ninja-build autoconf bison gperf autopoint help2man rsync parted qemu
 pip3 install --user meson xbstrap
-git clone https://github.com/qword-os2/qword /workspaces/qword
-git clone https://github.com/qword-os2/util-qword /workspaces/util-qword
